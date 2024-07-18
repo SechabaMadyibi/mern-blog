fd37f2666e5efd32ac8c7b59619d939861820710
sechaba <sechabamadyibi1@gmail.com>
2024-07-18 11:01:09 +0200

    create the footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Footer } from "flowbite-react";
 import { Link } from "react-router-dom";
+import {BsFacebook,BsInstagram,BsTwitter,BsGithub,BsDribbble} from "react-icons/bs"
 
 export default function FooterCom() {
   return (
     <Footer container className="border border-t-8 border-teal-500">
-      <div className="">
-        <div className="">
-          <div className="">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
             <Link
               to="/"
               className="self-centre whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
@@ -18,7 +19,58 @@ export default function FooterCom() {
               Blog
             </Link>
           </div>
-          <div className="grid grid-cols-2"></div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://book-store-app12.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Bookstore
+                </Footer.Link>
+                <Footer.Link href="http://localhost:5173/about">
+                  Sahand's Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Follow us" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/SechabaMadyibi/mern-blog"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href="#">Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms & Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="Sahand's blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook}/>
+            <Footer.Icon href="#" icon={BsInstagram}/>
+            <Footer.Icon href="#" icon={BsTwitter}/>
+            <Footer.Icon href="https://github.com/SechabaMadyibi/mern-blog" icon={BsGithub}/>
+            <Footer.Icon href="#" icon={BsDribbble}/>
+          </div>
         </div>
       </div>
     </Footer>
