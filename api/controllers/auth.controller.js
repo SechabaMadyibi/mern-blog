1987c3042d88e6d266579df834aa79c36fd86dd6
sechaba <sechabamadyibi1@gmail.com>
2024-07-19 08:51:29 +0200

    signin function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 
+//signup function
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -27,3 +28,6 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+//signin function
+
