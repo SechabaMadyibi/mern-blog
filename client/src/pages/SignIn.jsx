4e127c8cd4e9d576baadf2327070cdbf3ac74dbe
sechaba <sechabamadyibi1@gmail.com>
2024-07-22 08:23:12 +0200

    using redux for global states

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,17 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import React, { useState } from "react";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice.js";
 
 export default function SignIn() {
   const [formData, setData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -15,12 +21,11 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage("Please fill out all fields");
+      return dispatch(signInFailure('Please fill all the fields'))
     }
 
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch("api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -28,16 +33,15 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success == false) {
-       return setErrorMessage(data.message)
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
-      if (res.ok)
-      {
-        navigate('/')
+    
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
       }
     } catch (error) {
-      setErrorMessage(error.message);
-
+      dispatch(signInFailure(error.message));
     }
   };
 
@@ -60,7 +64,6 @@ export default function SignIn() {
         {/* right */}
         <div className="flex-1">
           <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-            
             <div className="">
               <Label value="Your email" />
 
@@ -81,15 +84,19 @@ export default function SignIn() {
                 onChange={handleChange}
               />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit" disabled= {loading}>
-            {
-              loading ? (
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
                 <>
-                <Spinner size='sm'/>
-                <span className="pl-3">loading...</span>
+                  <Spinner size="sm" />
+                  <span className="pl-3">loading...</span>
                 </>
-              ): 'Sign In' 
-            }
+              ) : (
+                "Sign In"
+              )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -108,4 +115,3 @@ export default function SignIn() {
     </div>
   );
 }
-
