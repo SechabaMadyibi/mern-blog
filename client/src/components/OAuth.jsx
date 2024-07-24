b538def5495ddad19f7df8d4fd53b596549ab76e
sechaba <sechabamadyibi1@gmail.com>
2024-07-24 08:09:11 +0200

    google signup/in button

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,16 @@
+import { Button } from "flowbite-react";
+import React from "react";
+import { AiFillGoogleCircle } from "react-icons/ai";
+
+export default function OAuth() {
+const handleGoogleClick = async () => {
+
+}
+
+  return (
+    <Button type="button" gradientDuoTone="pinkToOrange" outline onClick={handleGoogleClick}>
+      <AiFillGoogleCircle className="w-6 h-6 mr-2"/>
+      Continue with Google
+    </Button>
+  );
+}
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -7,6 +7,7 @@ import {
   signInSuccess,
   signInFailure,
 } from "../redux/user/userSlice.js";
+import OAuth from "../components/OAuth.jsx";
 
 export default function SignIn() {
   const [formData, setData] = useState({});
@@ -98,6 +99,7 @@ export default function SignIn() {
                 "Sign In"
               )}
             </Button>
+            <OAuth/>
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Don't have an acccount?</span>
